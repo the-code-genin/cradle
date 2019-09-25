9286ee18c71c78b9dec322b66a61f496f8b091f0
Iyiola-am <adekunle3317@gmail.com>
2019-09-25 19:33:56 +0100

    getDB method added to Model super class

--- vendor/cradle/Components/Model.php
+++ vendor/cradle/Components/Model.php
@@ -4,9 +4,8 @@ namespace Cradle\Components;
 /**
  * The base class for all models in the system.
  * All valid model should extend this class or any of its subclasses for uniformity between models.
- * The class is empty because models can have a different data source other than a database.
  */
 abstract class Model
 {
-	// Empty class
+	abstract protected function getDB();
 }
