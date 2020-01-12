30b4311c6374994a493d2be0e0b3a0563faa6054
Iyiola-am <adekunle3317@gmail.com>
2020-01-12 01:08:41 +0100

    Added sample app classes

---
+++ app/Models/SampleModel.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Models;
+
+use Cradle\Components\Model;
+
+/**
+ * This is just a sample model to illustrate how models are supposed to be created in cradle.
+ */
+class SampleModel extends Model
+{
+	//
+}
