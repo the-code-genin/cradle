06c9213e9cd873fc66176d2d6a1f52c4bfae34ef
Iyiola-am <adekunle3317@gmail.com>
2020-01-12 02:46:21 +0100

    Refactored app structure

--- framework/Components/Model.php
+++ framework/Components/Model.php
@@ -1,11 +1,23 @@
 <?php
 namespace Cradle\Components;
 
+use Illuminate\Database\Capsule\Manager;
+use Illuminate\Database\Eloquent\Model as EloquentModel;
+
 /**
  * The base class for all models in the system.
  * All valid model should extend this class or any of its subclasses for uniformity between models.
  */
-abstract class Model
+class Model extends EloquentModel
 {
-	abstract protected function getDB();
+	/**
+	 * Return a new new manager instance
+	 *
+	 * @return \Illuminate\Database\Capsule\Manager
+	 */
+	public static function getDB()
+	{
+		$manager = new Manager;
+		return $manager->getConnection();
+	}
 }
--- public/index.php
+++ public/index.php
@@ -102,6 +102,33 @@ set_error_handler('Cradle\Components\Logger::handleError');
 
 /**
  * ------------------------------------------------------------------
+ * Database connection
+ * ------------------------------------------------------------------
+ * 
+ * Set up a connection to the database
+ */
+
+$capsule = new Illuminate\Database\Capsule\Manager;
+$capsule->addConnection([
+	'driver' => getenv('DB_DRIVER'),
+	'host' => getenv('DB_HOST'),
+	'database' => getenv('DB_NAME'),
+	'username' => getenv('DB_USER'),
+	'password' => getenv('DB_PASS'),
+	'charset' => 'utf8',
+	'collation' => 'utf8_unicode_ci',
+	'prefix' => '',
+]);
+
+// Setup the Eloquent ORM
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
+
+
+
+/**
+ * ------------------------------------------------------------------
  * ROUTING
  * ------------------------------------------------------------------
  *
