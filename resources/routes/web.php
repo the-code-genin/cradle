0d3e01ee20fab3e9468bd5a1969a40d4d849c53b
Iyiola-am <adekunle3317@gmail.com>
2020-01-30 03:23:58 +0100

    New routes resource structure implemented

---
+++ resources/routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Slim\App;
+
+/**
+ * Routing rules are defined here.
+ */
+
+/** @var App $app The slim application instance. */
+$app;
+
+$app->get('/', function(RequestInterface $request, ResponseInterface $response, $args) {
+    $response->getBody()->write('Hello world!');
+    return $response->withHeader('Content-Type', 'text/plain');
+});
