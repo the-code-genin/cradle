9263f7beaac9eb6b98e9487a605885b438bedae4
Iyiola-am <adekunle3317@gmail.com>
2020-02-12 01:05:47 +0100

    CORS middleware implemented

---
+++ app/Middleware/CORSMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Middleware;
+
+use Cradle\MiddleWare;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+/**
+ * This middleware enables CORS support for the api.
+ */
+class CORSMiddleware extends MiddleWare
+{
+	public function __invoke(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $response = $handler->handle($request);
+        return $response->withHeader('Cache-Control', ['no-cache', 'must-revalidate'])
+            ->withHeader('Access-Control-Allow-Credentials', 'true')
+            ->withHeader('Access-Control-Max-Age', '86400')
+            ->withHeader('Access-Control-Allow-Headers', [
+                'Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization', 'X-Http-Method-Override'
+            ])
+            ->withHeader('Access-Control-Allow-Methods', ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'])
+            ->withHeader('Access-Control-Allow-Origin', '*');
+    }
+}
