ed09b9badcff09e2b11c29f6b298e0a51533208c
Iyiola-am <adekunle3317@gmail.com>
2020-02-15 10:34:55 +0100

    Fixed bugs in cron tasks script

--- cronTasks.php
+++ cronTasks.php
@@ -5,15 +5,17 @@ use Crunz\Schedule;
 // Require autoloader.
 require_once __DIR__ .'/vendor/autoload.php';
 
+// Define base directory.
+define('BASE_DIR', __DIR__);
 
 // Load environment values from the .env file if a .env file exists.
 if (file_exists(BASE_DIR . '/.env')) {
 	\Dotenv\Dotenv::createImmutable(BASE_DIR)->load();
 }
 
-
 // Set default timezone for app to UTC.
 date_default_timezone_set(getenv('TIME_ZONE') ? getenv('TIME_ZONE') : 'UTC');
+
 
 // Set the schedules.
 $schedule = new Schedule();
