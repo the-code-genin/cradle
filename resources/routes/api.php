f31e1850a8e44dac7803ead3a6f12027ebd89da3
Iyiola-am <adekunle3317@gmail.com>
2020-02-15 09:44:27 +0100

    Updated route files

--- resources/routes/api.php
+++ resources/routes/api.php
@@ -1,11 +1,14 @@
 <?php
 
+use Slim\App;
 use App\Controllers\Api\Home;
 use App\Middleware\CORSMiddleware;
 use Slim\Routing\RouteCollectorProxy;
 
 /**
  * Routing rules are defined here.
+ * 
+ * @var App $app
  */
 
 $app->group('/api/v0', function (RouteCollectorProxy $group) {
--- resources/routes/web.php
+++ resources/routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
+use Slim\App;
 use App\Controllers\Web\Home;
 
 /**
  * Routing rules are defined here.
+ * 
+ * @var App $app
  */
 
 $app->get('/', Home::class.':index');
