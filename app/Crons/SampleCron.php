1633aec3f09e8e80815e85bdae3ce0f7122e29d5
Iyiola-am <adekunle3317@gmail.com>
2020-02-15 12:10:12 +0100

    Sample cron added

---
+++ app/Crons/SampleCron.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Crons;
+
+use Cradle\Cron;
+
+class SampleCron extends Cron
+{
+    /**
+     * A sample cron that is run every minute and returns no values.
+     *
+     * @param object $params
+     * @return void
+     */
+    public function everyMinute(object $params): void
+    {
+        // Code
+    }
+}
