bd1d9aef84ab1b75509d4a2c8ec39a4101c02ad2
Mohammed Adekunle <adekunle3317@gmail.com>
2021-07-10 16:45:38 +0100

    Refactored CORS middleware class

--- Middleware/CORSMiddleware.php
+++ Middleware/CORSMiddleware.php
@@ -1,6 +1,7 @@
 <?php
 namespace Middleware;
 
+use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
@@ -8,9 +9,9 @@ use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 /**
  * This middleware enables CORS support for the API endpoints.
  */
-class CORSMiddleware
+class CORSMiddleware implements MiddlewareInterface
 {
-	public function __invoke(Request $request, RequestHandler $handler, array $args): Response
+	public function process(Request $request, RequestHandler $handler): Response
     {
         $response = $handler->handle($request);
         return $response->withHeader('Cache-Control', ['no-cache', 'must-revalidate'])
