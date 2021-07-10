be8f986bfd5a05243c7321263be200b63627d905
Mohammed Adekunle <adekunle3317@gmail.com>
2021-07-10 17:47:11 +0100

    Implemented signup route

---
+++ Controllers/AuthController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Controllers;
+
+use Models\User;
+use Lib\ApiResponse;
+use Valitron\Validator;
+use Lib\Errors\ServerError;
+use Lib\Errors\ConflictError;
+use Lib\Errors\InvalidFormDataError;
+use Lib\JWT;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class AuthController {
+    public static function signup(Request $request, Response $response, array $args): Response
+    {
+        // Get parsed body
+        $body = (object) $request->getParsedBody();
+
+
+        // Validate the user input
+        $v = new Validator($body);
+        $v->rule('required', ['name', 'email', 'password']);
+        $v->rule('email', ['email']);
+        $v->rule('lengthMin', ['password'], 6);
+
+        if (!$v->validate()) {
+            $response->getBody()->write(new InvalidFormDataError(array_shift($v->errors())[0]));
+            return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
+        } else if (User::where('email', $body->email)->count() != 0) {
+            $response->getBody()->write(new ConflictError('This email is taken.'));
+            return $response->withStatus(409)->withHeader('Content-Type', 'application/json');
+        }
+
+
+        // Create the user account
+        $user = new User;
+        try {
+            $user->name = $body->name;
+            $user->email = $body->email;
+            $user->password = password_hash($body->password, PASSWORD_DEFAULT);
+            $user->save();
+        } catch(\Exception $e) {
+            $response->getBody()->write(new ServerError($e->getMessage()));
+            return $response->withStatus(500)->withHeader('Content-Type', 'application/json');
+        }
+
+
+        // Return the response
+        $response->getBody()->write(new ApiResponse([
+            'data' => $user->fresh(),
+            'access_token' => JWT::generateAccessToken($user),
+            'token_type' => 'bearer'
+        ]));
+        return $response->withStatus(200)->withHeader('Content-Type', 'application/json');
+    }
+}
