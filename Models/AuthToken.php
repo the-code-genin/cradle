35dad601ffd65ed6d5ebc5230258689397b97f45
Mohammed Adekunle <adekunle3317@gmail.com>
2021-07-10 16:38:22 +0100

    Added auth token model

---
+++ Models/AuthToken.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class AuthToken extends Model {
+    /**
+     * Get the owning user.
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
--- Models/User.php
+++ Models/User.php
@@ -3,8 +3,12 @@
 namespace Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Model
 {
-    //
+    public function authTokens(): HasMany
+    {
+        return $this->hasMany(AuthToken::class, 'user_id');
+    }
 }
