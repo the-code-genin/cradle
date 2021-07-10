f745f930ca96aeb54fdb5ccb5fafb8f3f003f0c6
Mohammed Adekunle <adekunle3317@gmail.com>
2021-07-10 17:57:01 +0100

    Refactored auth controller

--- Controllers/AuthController.php
+++ Controllers/AuthController.php
@@ -27,10 +27,10 @@ class AuthController {
         $v->rule('lengthMin', ['password'], 6);
 
         if (!$v->validate()) {
-            $response->getBody()->write(new InvalidFormDataError(array_shift($v->errors())[0]));
+            $response->getBody()->write((string) new InvalidFormDataError(array_shift($v->errors())[0]));
             return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
         } else if (User::where('email', $body->email)->count() != 0) {
-            $response->getBody()->write(new ConflictError('This email is taken.'));
+            $response->getBody()->write((string) new ConflictError('This email is taken.'));
             return $response->withStatus(409)->withHeader('Content-Type', 'application/json');
         }
 
@@ -43,13 +43,13 @@ class AuthController {
             $user->password = password_hash($body->password, PASSWORD_DEFAULT);
             $user->save();
         } catch(\Exception $e) {
-            $response->getBody()->write(new ServerError($e->getMessage()));
+            $response->getBody()->write((string) new ServerError($e->getMessage()));
             return $response->withStatus(500)->withHeader('Content-Type', 'application/json');
         }
 
 
         // Return the response
-        $response->getBody()->write(new ApiResponse([
+        $response->getBody()->write((string) new ApiResponse([
             'data' => $user->fresh(),
             'access_token' => JWT::generateAccessToken($user),
             'token_type' => 'bearer'
@@ -69,7 +69,7 @@ class AuthController {
         $v->rule('email', ['email']);
 
         if (!$v->validate()) {
-            $response->getBody()->write(new InvalidFormDataError(array_shift($v->errors())[0]));
+            $response->getBody()->write((string) new InvalidFormDataError(array_shift($v->errors())[0]));
             return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
         }
 
@@ -77,20 +77,38 @@ class AuthController {
         // Get and validate the user account
         $user = User::where('email', $body->email)->first();
         if ($user == null) {
-            $response->getBody()->write(new NotFoundError('No user was found with the email and password combination.'));
+            $response->getBody()->write((string) new NotFoundError('No user was found with the email and password combination.'));
             return $response->withStatus(404)->withHeader('Content-Type', 'application/json');
         } else if (!password_verify($user->password, $body->password)) {
-            $response->getBody()->write(new NotFoundError('No user was found with the email and password combination.'));
+            $response->getBody()->write((string) new NotFoundError('No user was found with the email and password combination.'));
             return $response->withStatus(404)->withHeader('Content-Type', 'application/json');
         }
 
 
         // Return the response
-        $response->getBody()->write(new ApiResponse([
+        $response->getBody()->write((string) new ApiResponse([
             'data' => $user->fresh(),
             'access_token' => JWT::generateAccessToken($user),
             'token_type' => 'bearer'
         ]));
         return $response->withStatus(200)->withHeader('Content-Type', 'application/json');
+    }
+
+    public static function logout(Request $request, Response $response, array $args): Response
+    {
+        // $validator = new Validator($request->);
+        return $response;
+    }
+
+    public static function getMe(Request $request, Response $response, array $args): Response
+    {
+        // $validator = new Validator($request->);
+        return $response;
+    }
+
+    public static function updateMe(Request $request, Response $response, array $args): Response
+    {
+        // $validator = new Validator($request->);
+        return $response;
     }
 }
