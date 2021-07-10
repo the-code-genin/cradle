7492a83edefe616e320528b8f305d5b28d0b4099
Mohammed Adekunle <adekunle3317@gmail.com>
2021-07-10 15:56:05 +0100

    Refactored phinx config file

--- phinx.php
+++ phinx.php
@@ -1,27 +1,27 @@
 <?php
 
-use Cradle\Migration;
+use Dotenv\Dotenv;
 
-require __DIR__ . '/bootstrap.php';
+require __DIR__ . '/vendor/autoload.php';
+
+// Load env variables
+if (file_exists(__DIR__ . '/.env')) Dotenv::createImmutable(__DIR__)->load();
 
 return
 [
     'paths' => [
-        'migrations' => __DIR__ . '/db/migrations',
-        'seeds' => __DIR__ . '/db/seeds'
+        'migrations' => __DIR__ . '/migrations'
     ],
-    'migration_base_class' => Migration::class,
     'environments' => [
         'default_migration_table' => 'phinxlog',
         'default_database' => 'development',
         'development' => [
-            'adapter' => getenv('DB_DRIVER') ? getenv('DB_DRIVER') : 'mysql',
-            'host' => getenv('DB_HOST') ? getenv('DB_HOST') : 'localhost',
-            'name' => getenv('DB_NAME') ? getenv('DB_NAME') : 'test',
-            'user' => getenv('DB_USERNAME') ? getenv('DB_USERNAME') : 'root',
-            'pass' => getenv('DB_PASSWORD') ? getenv('DB_PASSWORD') : '',
-            'port' => getenv('DB_PORT') ? getenv('DB_PORT') : '3306',
-            'charset' => 'utf8',
+            'adapter' => getenv('DB_DRIVER'),
+            'host' => getenv('DB_HOST'),
+            'name' => getenv('DB_NAME'),
+            'user' => getenv('DB_USERNAME'),
+            'pass' => getenv('DB_PASSWORD'),
+            'port' => getenv('DB_PORT')
         ]
     ],
     'version_order' => 'creation'
