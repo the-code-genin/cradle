3ef2f36bb03ba3f4500cbe0ea18df0561a53215f
Mohammed Adekunle <adekunle3317@gmail.com>
2021-07-10 17:03:30 +0100

    Added not found error

---
+++ Lib/Errors/NotFoundError.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Lib\Errors;
+
+class NotFoundError extends ApplicationError {
+    public function __construct(string $message = 'The resource you were looking for was not found on this server.')
+    {
+        parent::__construct(404, 'NotFoundError', $message);
+    }
+}
