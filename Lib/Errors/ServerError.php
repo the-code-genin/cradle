3300266c7f7809c1676a5ed101eb72a650cc870f
Mohammed Adekunle <adekunle3317@gmail.com>
2021-07-10 17:04:43 +0100

    Added server error class

---
+++ Lib/Errors/ServerError.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Lib\Errors;
+
+class ServerError extends ApplicationError {
+    public function __construct(string $message = 'Server Error!')
+    {
+        parent::__construct(500, 'ServerError', $message);
+    }
+}
