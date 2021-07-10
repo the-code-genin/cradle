581c7cea6d5de0191b822d6703421cc68111d4c4
Mohammed Adekunle <adekunle3317@gmail.com>
2021-07-10 16:19:41 +0100

    Refactored middleware structure

---
+++ Middleware/CORSMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+namespace Middleware;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+
+/**
+ * This middleware enables CORS support for the API endpoints.
+ */
+class CORSMiddleware
+{
+	public function __invoke(Request $request, RequestHandler $handler, array $args): Response
+    {
+        $response = $handler->handle($request);
+        return $response->withHeader('Cache-Control', ['no-cache', 'must-revalidate'])
+            ->withHeader('Access-Control-Allow-Credentials', 'true')
+            ->withHeader('Access-Control-Max-Age', '86400')
+            ->withHeader('Access-Control-Allow-Headers', [
+                'Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization', 'X-Http-Method-Override', 'Cookie'
+            ])
+            ->withHeader('Access-Control-Allow-Methods', ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'])
+            ->withHeader('Access-Control-Allow-Origin', '*');
+    }
+}
