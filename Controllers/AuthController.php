386c18506bc8f16d4cbd3de473a0b95e3e051a31
Mohammed Adekunle <adekunle3317@gmail.com>
2021-07-10 17:53:49 +0100

    Added login route

--- Controllers/AuthController.php
+++ Controllers/AuthController.php
@@ -2,13 +2,14 @@
 
 namespace Controllers;
 
+use Lib\JWT;
 use Models\User;
 use Lib\ApiResponse;
 use Valitron\Validator;
 use Lib\Errors\ServerError;
 use Lib\Errors\ConflictError;
 use Lib\Errors\InvalidFormDataError;
-use Lib\JWT;
+use Lib\Errors\NotFoundError;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
@@ -20,7 +21,7 @@ class AuthController {
 
 
         // Validate the user input
-        $v = new Validator($body);
+        $v = new Validator((array) $body);
         $v->rule('required', ['name', 'email', 'password']);
         $v->rule('email', ['email']);
         $v->rule('lengthMin', ['password'], 6);
@@ -44,6 +45,43 @@ class AuthController {
         } catch(\Exception $e) {
             $response->getBody()->write(new ServerError($e->getMessage()));
             return $response->withStatus(500)->withHeader('Content-Type', 'application/json');
+        }
+
+
+        // Return the response
+        $response->getBody()->write(new ApiResponse([
+            'data' => $user->fresh(),
+            'access_token' => JWT::generateAccessToken($user),
+            'token_type' => 'bearer'
+        ]));
+        return $response->withStatus(201)->withHeader('Content-Type', 'application/json');
+    }
+
+    public static function login(Request $request, Response $response, array $args): Response
+    {
+        // Get parsed body
+        $body = (object) $request->getParsedBody();
+
+
+        // Validate the user input
+        $v = new Validator((array) $body);
+        $v->rule('required', ['email', 'password']);
+        $v->rule('email', ['email']);
+
+        if (!$v->validate()) {
+            $response->getBody()->write(new InvalidFormDataError(array_shift($v->errors())[0]));
+            return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
+        }
+
+
+        // Get and validate the user account
+        $user = User::where('email', $body->email)->first();
+        if ($user == null) {
+            $response->getBody()->write(new NotFoundError('No user was found with the email and password combination.'));
+            return $response->withStatus(404)->withHeader('Content-Type', 'application/json');
+        } else if (!password_verify($user->password, $body->password)) {
+            $response->getBody()->write(new NotFoundError('No user was found with the email and password combination.'));
+            return $response->withStatus(404)->withHeader('Content-Type', 'application/json');
         }
 
 
