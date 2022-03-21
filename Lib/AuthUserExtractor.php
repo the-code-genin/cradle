1c5e75ef7dbb32e1dad7ab920ceb289f924dd4b9
the-code-genin <adekunle3317@gmail.com>
2022-03-21 19:28:27 +0100

    Refactored auth user extractor

--- Lib/AuthUserExtractor.php
+++ Lib/AuthUserExtractor.php
@@ -2,8 +2,9 @@
 
 namespace Lib;
 
+use Database\Entities\User;
+use Database\Repositories\Users;
 use Lib\JWT;
-use Models\User;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 class AuthUserExtractor
@@ -20,8 +21,7 @@ class AuthUserExtractor
             // Verify the bearer token header is set
             $header = $request->getHeader('Authorization');
             if (count($header) != 1) throw new \Exception('Authorization header not set!');
-            else if (!preg_match("/^Bearer (.+)$/i", $header[0])) throw new \Exception('Bearer token not set!');
-
+            else if (!preg_match("/^Bearer (.+)$/i", $header[0])) throw new \Exception('Bad/Invalid bearer token');
 
             // Parse the bearer token
             preg_match("/^Bearer (.+)$/i", $header[0], $matches);
@@ -31,11 +31,10 @@ class AuthUserExtractor
             /** @var mixed $id */
             $id = $parsedToken->claims()->get('jti');
 
-
             // Validate the user object
-            $user = User::where('id', $id)->first();
-            if ($user == null) throw new \Exception('User is not Authenticated');
-            else if ($user->authTokens()->where('auth_tokens.token', $token)->count() != 0) throw new \Exception('User is not Authenticated');
+            $user = Users::getUserById($id);
+            if ($user == null) throw new \Exception('Bad/Invalid bearer token');
+            else if (Users::checkUserHasAuthToken($user->id, $token)) throw new \Exception('Expired bearer token');
             else if ($user->status != 'active') throw new \Exception('User is banned from using the platform');
 
 
